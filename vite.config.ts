cd71cd815441e6a9976c6312f70e000c8f68f2b8
Artem Korolev <artkor@live.ru>
2023-11-01 14:59:24 +0200

    SPA fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from "vite-plugin-static-copy"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "dist/index.html",
+          dest: ".",
+          rename: () => "404.html",
+        },
+      ],
+    })
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
-    port: 8080
-  }
-})
+    port: 8080,
+  },
+});
