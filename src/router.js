5b31d4c668d010b299997d000c53d63003d4f735
Artem Korolev <artkor@live.ru>
2023-11-01 13:04:21 +0200

    styling menu

--- src/router.js
+++ src/router.js
@@ -1,25 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import Donate from './views/Donate.vue'
-import ThankYou from './views/ThankYou.vue'
+import ThankYou from "./views/ThankYou.vue"
+import MobileFirst from "./views/MobileFirst.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/donate',
-    name: 'Donate',
-    component: Donate
+    path: "/donate",
+    name: "Donate",
+    component: Donate,
   },
   {
-    path: '/thank-you',
-    name: 'ThankYou',
-    component: ThankYou
-  }
-]
+    path: "/thank-you",
+    name: "ThankYou",
+    component: ThankYou,
+  },
+  {
+    path: "/mobile-first",
+    name: "MobileFirst",
+    component: MobileFirst,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ src/views/MobileFirst.vue
@@ -0,0 +1,18 @@
+<!-- MobileFirst.vue -->
+<template>
+    <div>
+        <TopMenu />
+        <h1>Mobile First</h1>
+    </div>
+</template>
+
+<script>
+import TopMenu from '../components/TopMenu.vue';
+
+export default {
+    name: 'MobileFirst',
+    components: {
+        TopMenu
+    }
+}
+</script>
