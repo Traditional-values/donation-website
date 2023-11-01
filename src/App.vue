89cb402db9cb7db04c22f80b223c69f318ef48bf
Artem <artem-korolev@users.noreply.github.com>
2023-11-01 18:51:49 +0200

    Update logo

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/IMG_20231101_184733.jpg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="Hello Traditional Values 2023!" />
